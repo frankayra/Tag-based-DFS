ddf412b386f2bfd7633aa92a9200e6c0a159a79d
Francisco O. Ayra Cáceres <franciscoayracaceress@gmail.com>
2024-05-18 17:06:56 -0400

    Parser cleaned

--- Server/parser.py
+++ Server/parser.py
@@ -35,66 +35,15 @@ def exec_command(prompt:str, actions:dict):
     except Exception as e:
         return f"Formato incorrecto: {e}"
 
-    # match match.group('command'):
-    #     case 'delete':
-    #         return delete_tags(arg1_list)
-    #     case 'list':
-    #         return list_(arg1_list)
-    #     case 'add':
-    #         if not arg2_match: return "Formato incorrecto"
-    #         fill_list(arg2_match, arg2_list)
-    #         return add(arg1_list, arg2_list)
-    #     case 'add-tags':
-    #         if not arg2_match: return "Formato incorrecto"
-    #         fill_list(arg2_match, arg2_list)
-    #         return add_tags(arg1_list, arg2_list)
-    #     case 'delete-tags':
-    #         if not arg2_match: return "Formato incorrecto"
-    #         fill_list(arg2_match, arg2_list)
-    #         return delete_tags(arg1_list, arg2_list)
-    #     case _:
-    #         return "Formato incorrecto"
+def RunPrompt(commands: dict):
+    print("Consola de comandos\n" + 
+            "Comandos permitidos:\n" +
+            "- add file-list tag-list\n" +
+            "- delete tag-query\n" +
+            "- list tag-query\n" +
+            "- add-tags tag-query tag-list\n" +
+            "- delete tag-query tag-list\n")
 
-def add(files_list, tags_list):
-    print(f"argumento 1: {files_list}\n argumento 2: {tags_list}")
-    pass
-def delete(tag_query):
-    print(f"argumento 1: {tag_query}")
-    pass
-def list_(tag_query):
-    print(f"argumento 1: {tag_query}")
-    pass
-def add_tags(tag_query, tags_list):
-    print(f"argumento 1: {tag_query}\n argumento 2: {tags_list}")
-    pass
-def delete_tags(tag_query, tags_list):
-    print(f"argumento 1: {tag_query}\n argumento 2: {tags_list}")
-    pass
-
-print("Consola de comandos\n" + 
-        "Comandos permitidos:\n" +
-        "- add file-list tag-list\n" +
-        "- delete tag-query\n" +
-        "- list tag-query\n" +
-        "- add-tags tag-query tag-list\n" +
-        "- delete tag-query tag-list\n")
-commands = {
-    "add": add,
-    "delete": delete,
-    "list": list_,
-    "add-tags": add_tags,
-    "delete-tags": delete_tags,
-}
-while 1:
-    command = input(">>> ")
-    exec_command(command, commands)
-command_example_1 = "add file_1.txt|resource_2do.pdf"
-command_example_2 = "add file1.txt fotos_con_mi_madre|Vacaciones"
-command_example_3 = ""
-command_example_4 = ""
-command_example_5 = ""
-command_example_6 = ""
-command_example_7 = ""
-command_example_8 = ""
-command_example_9 = ""
-command_example_10 = ""
+    while 1:
+        command = input(">>> ")
+        exec_command(command, commands)
