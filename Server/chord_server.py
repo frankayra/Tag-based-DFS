d880200727737e42a83213e162c4151ff00970a4
Francisco O. Ayra Cáceres <franciscoayracaceress@gmail.com>
2024-07-16 16:35:24 -0400

    Chord server started

---
+++ Server/chord_server.py
@@ -0,0 +1,55 @@
+
+import socket
+import hashlib
+
+M = 160
+
+class ChordNode:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.successor = self
+        self.predecessor = None
+        self.finger_table = [self] * M
+
+    def find_successor(self, key):
+        if self.id == self.successor.id or self.id < self.successor.id:
+            if self.id < key <= self.successor.id:
+                return self.successor
+            else:
+                return self.finger_table_closest_node(key).find_successor(key)
+        else:
+            if self.id < key or key <= self.successor.id:
+                return self.successor
+            else:
+                return self.finger_table_closest_node(key).find_successor(key)
+
+    def finger_table_closest_node(self, key):
+        for i in range(M-1, -1):
+            if self.id < self.finger_table[i].id < key:
+                return self.finger_table[i]
+        return self
+
+    def join(self, existing_node):
+        if existing_node:
+            self.successor = existing_node.find_successor(self.id)
+        else:
+            self.predecessor = self
+            self.successor = self
+            for i in range(M):
+                self.finger_table[i] = self
+
+    def stabilize(self):
+        x = self.successor.predecessor
+        if x and (self.id < x.id < self.successor.id):
+            self.successor = x
+        self.successor.notify(self)
+
+    def notify(self, node):
+        if not self.predecessor or (self.predecessor.id < node.id < self.id):
+            self.predecessor = node
+
+    def fix_fingers(self):
+        for i in range(M):
+            self.finger_table[i] = self.find_successor((self.id + 2**i) % (2**M))
+
