d5fa214e99947781a0403ae1b71fa3db67d00366
Francisco O. Ayra Cáceres <franciscoayracaceress@gmail.com>
2024-08-01 12:35:46 -0400

    refurbishments

--- Server/chord_server.py
+++ Server/chord_server.py
@@ -8,12 +8,13 @@ class ChordNode:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.id = create_node_id(ip, port)
         self.successor = self
         self.predecessor = None
         self.finger_table = [self] * M
 
     def find_successor(self, key):
-        if self.id == self.successor.id or self.id < self.successor.id:
+        if self.id <= self.successor.id:
             if self.id < key <= self.successor.id:
                 return self.successor
             else:
@@ -25,7 +26,7 @@ class ChordNode:
                 return self.finger_table_closest_node(key).find_successor(key)
 
     def finger_table_closest_node(self, key):
-        for i in range(M-1, -1):
+        for i in range(M-1, -1, -1):
             if self.id < self.finger_table[i].id < key:
                 return self.finger_table[i]
         return self
@@ -53,3 +54,9 @@ class ChordNode:
         for i in range(M):
             self.finger_table[i] = self.find_successor((self.id + 2**i) % (2**M))
 
+
+
+
+def create_node_id(ip, port):
+    hash_object = hashlib.sha1(f'{ip}:{port}'.encode())
+    return int(hash_object.hexdigest(), 16) % (2 ** M)
